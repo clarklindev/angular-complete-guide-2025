dbd2eda567a821fc0e1df51c2aff27b4e886cb7d
clarklindev <clarklindev@gmail.com>
2025-09-14 15:44:10 +0200

    06. Components & Templates - Component Cleanup with DestroyRef

--- 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  DestroyRef,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,14 +13,15 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit, OnDestroy {
+export class ServerStatusComponent implements OnInit {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
-  private interval?: ReturnType<typeof setInterval>;
+
+  private destroyRef = inject(DestroyRef);
 
   constructor() {}
 
   ngOnInit() {
-    this.interval = setInterval(() => {
+    const interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd < 0.5) {
         this.currentStatus = 'online';
@@ -24,9 +31,9 @@ export class ServerStatusComponent implements OnInit, OnDestroy {
         this.currentStatus = 'unknown';
       }
     }, 5000);
-  }
 
-  ngOnDestroy(): void {
-    clearTimeout(this.interval);
+    this.destroyRef.onDestroy(() => {
+      clearInterval(interval);
+    });
   }
 }
