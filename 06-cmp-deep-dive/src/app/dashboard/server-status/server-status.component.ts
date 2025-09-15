709df816aefd965997ae74586f7a8f40e738fd75
clarklindev <clarklindev@gmail.com>
2025-09-15 08:06:42 +0200

    06. Components & Templates - effect() - Making Sense of Signal Effects

--- 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/server-status/server-status.component.ts
@@ -1,9 +1,11 @@
 import {
   Component,
   DestroyRef,
+  effect,
   inject,
   // OnDestroy,
   OnInit,
+  signal,
 } from '@angular/core';
 
 @Component({
@@ -16,23 +18,28 @@ import {
 
 //implements OnDestroy
 export class ServerStatusComponent implements OnInit {
-  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  currentStatus = signal<'online' | 'offline' | 'unknown'>('offline');
   // private interval?: ReturnType<typeof setInterval>;
 
   private destroyRef = inject(DestroyRef);
 
-  constructor() {}
+  constructor() {
+    //using a signal inside effect(), angular will setup a subscription
+    effect(() => {
+      console.log(this.currentStatus());
+    });
+  }
 
   ngOnInit() {
     // this.interval =
     const interval = setInterval(() => {
       const rnd = Math.random();
       if (rnd < 0.5) {
-        this.currentStatus = 'online';
+        this.currentStatus.set('online');
       } else if (rnd < 0.9) {
-        this.currentStatus = 'offline';
+        this.currentStatus.set('offline');
       } else {
-        this.currentStatus = 'unknown';
+        this.currentStatus.set('unknown');
       }
     }, 5000);
 
