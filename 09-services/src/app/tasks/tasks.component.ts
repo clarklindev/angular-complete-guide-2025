1289b1315a5fcf27ed82a85be960d30a1f079499
clarklindev <clarklindev@gmail.com>
2025-09-17 16:04:17 +0200

    09.services - Using Custom DI Tokens & Providers

--- 09-services/src/app/tasks/tasks.component.ts
+++ 09-services/src/app/tasks/tasks.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TasksListComponent } from './tasks-list/tasks-list.component';
+// import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   imports: [NewTaskComponent, TasksListComponent],
+  // providers: [TasksService], //with element injector - each instance of '<app-tasks>' will have its own service instance
 })
 export class TasksComponent {}
