da5ebf09fc87065cefa2ca03f7f9799a29876b6a
clarklindev <clarklindev@gmail.com>
2025-09-17 16:10:12 +0200

    09.services - Preparing A Non-Class Value For Injection

--- 09-services/src/app/tasks/task.model.ts
+++ 09-services/src/app/tasks/task.model.ts
@@ -6,3 +6,25 @@ export interface Task {
   description: string;
   status: TaskStatus;
 }
+
+export const TaskStatusOptions: {
+  value: 'open' | 'in-progress' | 'done';
+  taskStatus: TaskStatus;
+  text: string;
+}[] = [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open',
+  },
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'In-Progress',
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Completed',
+  },
+];
