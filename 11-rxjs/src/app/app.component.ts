fafee9c01d919c45cc87af51cd863de08703b2c3
clarklindev <clarklindev@gmail.com>
2025-09-18 09:20:04 +0200

    11.rxjs - toObservable() - Coverting Signals to Observables

--- 11-rxjs/src/app/app.component.ts
+++ 11-rxjs/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, DestroyRef, inject, OnInit } from '@angular/core';
-import { interval, map } from 'rxjs';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { toObservable } from '@angular/core/rxjs-interop';
+// import { interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,9 @@ import { interval, map } from 'rxjs';
   templateUrl: './app.component.html',
 })
 export class AppComponent implements OnInit {
+  clickCount = signal(0);
+  clickCount$ = toObservable(this.clickCount);
+
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
@@ -14,15 +18,25 @@ export class AppComponent implements OnInit {
     // const subscription = interval(1000).subscribe({
     //   next: (val) => console.log(val),
     // });
-
     //short-hand method for subscribe()
     //NOTE: using the rxjs 'pipe()' operator
-    const subscription = interval(1000)
-      .pipe(map((val) => val * 2))
-      .subscribe((val) => console.log(val));
+    // const subscription = interval(1000)
+    //   .pipe(map((val) => val * 2))
+    //   .subscribe((val) => console.log(val));
+    // this.destroyRef.onDestroy(() => {
+    //   subscription.unsubscribe();
+    // });
+
+    const subscription = this.clickCount$.subscribe({
+      next: (val) => console.log(`click button ${this.clickCount()} times`),
+    });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
     });
+  }
+
+  onClick() {
+    this.clickCount.update((prevCount) => prevCount + 1);
   }
 }
