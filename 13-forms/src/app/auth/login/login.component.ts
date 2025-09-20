81a363f00b1ef8d0b5786ae87b9c2cf3f1401644
clarklindev <clarklindev@gmail.com>
2025-09-20 19:34:14 +0200

    13.forms - template driven - Updating Form Values Programmatically

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -20,6 +20,19 @@ export class LoginComponent {
 
   constructor() {
     afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-login-form');
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        // this.form().setValue({
+        //   email: savedEmail,
+        //   password: '',
+        // });
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+
       const subscription = this.form()
         .valueChanges?.pipe(debounceTime(500))
         .subscribe({
