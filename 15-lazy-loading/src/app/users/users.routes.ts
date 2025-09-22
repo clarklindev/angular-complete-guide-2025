742bebd047914840c5a8a4d7c4b5c01ad43fea74
clarklindev <clarklindev@gmail.com>
2025-09-22 18:23:43 +0200

    15.lazy-loading - routing-based - Using Lazy Loading & Routing to Lazy-load Services

--- 15-lazy-loading/src/app/users/users.routes.ts
+++ 15-lazy-loading/src/app/users/users.routes.ts
@@ -6,26 +6,33 @@ import {
 } from '../tasks/new-task/new-task.component';
 
 import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
+import { TasksService } from '../tasks/tasks.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    // loadComponent: () =>
-    //   import('../tasks/tasks.component').then((mod) => mod.TasksComponent), //'mod' contains the thing exported by the file
-    component: TasksComponent,
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
-    },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage],
+    providers: [TasksService],
+    children: [
+      {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'full',
+      },
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        // loadComponent: () =>
+        //   import('../tasks/tasks.component').then((mod) => mod.TasksComponent), //'mod' contains the thing exported by the file
+        component: TasksComponent,
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage],
+      },
+    ],
   },
 ];
