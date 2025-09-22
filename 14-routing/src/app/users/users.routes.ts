979b08996e9a2c205f7e1b39404dbaf340f52526
clarklindev <clarklindev@gmail.com>
2025-09-22 16:28:29 +0200

    14.routing - runGuardsAndResolvers: 'paramsOrQueryParamsChange' - Controlling Route Resolver Execution

--- 14-routing/src/app/users/users.routes.ts
+++ 14-routing/src/app/users/users.routes.ts
@@ -1,18 +1,21 @@
 import { Routes } from '@angular/router';
-import { TasksComponent } from '../tasks/tasks.component';
+
+import { TasksComponent, resolveUserTasks } from '../tasks/tasks.component';
 import { NewTaskComponent } from '../tasks/new-task/new-task.component';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'tasks',
-    //pathMatch:'prefix' takes current 'path' combined with parent 'path' and checks if url starts with... the path
-    //pathMatch: 'full' takes current 'path' combined with parent 'path' and checks if url is EXACTLY the path
     pathMatch: 'full',
   },
   {
-    path: 'tasks', //<your-domain>/users/<uid>/tasks
+    path: 'tasks', // <your-domain>/users/<uid>/tasks
     component: TasksComponent,
+    runGuardsAndResolvers: 'paramsOrQueryParamsChange', //this fixes the default behavior is that resolver function re-executes if route parameter changes but NOT if query parameter changes
+    resolve: {
+      userTasks: resolveUserTasks,
+    },
   },
   {
     path: 'tasks/new',
