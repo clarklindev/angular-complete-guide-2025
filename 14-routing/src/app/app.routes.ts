40ee13b0820d149987a4804b80c1117a0e03ec95
clarklindev <clarklindev@gmail.com>
2025-09-22 16:45:39 +0200

    14.routing - Introducing Route Guards

--- 14-routing/src/app/app.routes.ts
+++ 14-routing/src/app/app.routes.ts
@@ -1,4 +1,10 @@
-import { Routes } from '@angular/router';
+import {
+  CanMatch,
+  CanMatchFn,
+  RedirectCommand,
+  Router,
+  Routes,
+} from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import {
   resolveTitle,
@@ -7,6 +13,20 @@ import {
 } from './users/user-tasks/user-tasks.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { routes as userRoutes } from './users/users.routes';
+import { inject } from '@angular/core';
+
+const dummyCanMatch: CanMatchFn = (route, segment) => {
+  //return 'true' to allow access 'false' to deny or observable that returns a boolean or redirect command
+  const router = inject(Router);
+
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5) {
+    return true;
+  }
+
+  //param of type UrlTree
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+};
 
 export const routes: Routes = [
   {
@@ -19,6 +39,9 @@ export const routes: Routes = [
     path: 'users/:userId', //<your-domain>/users/<uid>
     component: UserTasksComponent,
     children: userRoutes,
+
+    //route guard 'can...' keyword
+    canMatch: [dummyCanMatch],
 
     //adding static data to routes - app.config.ts requires withComponentInputBinding(),
     //any data key eg. 'message' will be provided as an input to the component for the route
