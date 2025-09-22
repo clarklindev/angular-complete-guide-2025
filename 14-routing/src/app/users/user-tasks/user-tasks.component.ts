700aae95781ea9c3739e6df1fcb580c3625d00aa
clarklindev <clarklindev@gmail.com>
2025-09-22 11:07:55 +0200

    14.routing - Route Links & Relative Links

--- 14-routing/src/app/users/user-tasks/user-tasks.component.ts
+++ 14-routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -7,14 +7,19 @@ import {
   OnInit,
 } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, ParamMap, RouterOutlet } from '@angular/router';
+import {
+  ActivatedRoute,
+  ParamMap,
+  RouterOutlet,
+  RouterLink,
+} from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
 })
 export class UserTasksComponent implements OnInit {
   //name an input with the same as the dynamic url segment
