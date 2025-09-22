a99788b9cffe9acb400f1616d6b82b920eedc94c
clarklindev <clarklindev@gmail.com>
2025-09-22 12:02:44 +0200

    14.routing - Loading Data Based On Route Parameters In Child Routes

--- 14-routing/src/app/tasks/tasks.component.ts
+++ 14-routing/src/app/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -13,5 +14,10 @@ import { Task } from './task/task.model';
 export class TasksComponent {
   //to get access need to set app.config.ts -> withRouterConfig({paramsInheritanceStrategy: 'always'});
   userId = input.required<string>();
-  userTasks: Task[] = [];
+
+  private tasksService = inject(TasksService);
+
+  userTasks = computed(() =>
+    this.tasksService.allTasks().filter((task) => task.userId === this.userId())
+  );
 }
